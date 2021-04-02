aa063d251f71833e5d881a1b37a1a5fe896bb7cf
mt1107056 <mtro0818@gmail.com>
2021-04-02 14:47:30 +0900

    Itemモデルのテスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png', content_type: 'image/png')
+    end
+    association :user
+    name              {"kamera"}
+    description       {"aaa"}
+    price             {1000}
+    category_id       {1}
+    status_id         {1}
+    cost_id           {1}
+    prefecture_id     {1}
+    delivery_id       {1}  
   end
 end
